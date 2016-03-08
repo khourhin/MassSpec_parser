2070a4765a013f3c3e1f759dff23fcc9c19b97c1
Etienne Kornobis <ekornobis@gmail.com>
2016-03-08 17:56:23 +0100

    minor Change static settting

--- MassSpec_web/MassSpec_web/settings.py
+++ MassSpec_web/MassSpec_web/settings.py
@@ -120,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'compare_compositions/static')
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'user_data')
